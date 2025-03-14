612d3188ddf7feaba304caea95869ae4085d1a3e
Jacob Porciuncula <jacobporci@gmail.com>
2025-03-14 20:58:50 +0800

    feat: initial UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,133 @@
-import Image from "next/image";
+import { HTMLProps } from "react";
+import { Edit2, Trash2 } from "react-feather";
 
+const Section = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: HTMLProps<HTMLElement>["className"];
+}) => <div className={`col-span-1 p-10 gap-y-4 ${className}`}>{children}</div>;
+
+const FormGrid = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: HTMLProps<HTMLElement>["className"];
+}) => (
+  <div className={`grid grid-cols-1 md:grid-cols-2 gap-x-5 ${className}`}>
+    {children}
+  </div>
+);
+
+const Field = ({ label, name }: { label: string; name: string }) => (
+  <div className="col-span-1 mt-5">
+    <label htmlFor="name" className="uppercase text-sm">
+      {label}
+    </label>
+    <input
+      className="border-1 border-gray-400 rounded-sm p-2 w-full"
+      type="text"
+      id={name}
+      name={name}
+    />
+  </div>
+);
+
+const SubHeader = ({ label }: { label: string }) => (
+  <h2 className="text-md uppercase font-bold border-b-gray-300 border-b-1 mt-5 pb-2">
+    {label}
+  </h2>
+);
+
+const TableHeader = ({ children }: { children: string }) => (
+  <th className="text-left">{children}</th>
+);
+
+const TableRow = ({ children }: { children: React.ReactNode[] }) => (
+  <tr className="border-b-1 border-gray-300">{children}</tr>
+);
+
+const TableCell = ({ children }: { children: string | React.ReactNode }) => (
+  <td className="text-left py-3">{children}</td>
+);
+const Actions = () => (
+  <div className="flex gap-2 text-gray-400">
+    <button>
+      <Edit2 />
+    </button>
+    <button>
+      <Trash2 />
+    </button>
+  </div>
+);
+
+const Button = ({
+  children,
+  className,
+}: {
+  children: string;
+  className?: HTMLProps<HTMLElement>["className"];
+}) => (
+  <button
+    className={`col-span-1 shadow-1 p-5 uppercase border-1 border-gray-300 shadow-lg font-bold rounded-sm ${className}`}
+  >
+    {children}
+  </button>
+);
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 min-h-screen text-[#aeaeae]">
+      <Section>
+        <h1 className="text-3xl font-extrabold text-black">Referral Builder</h1>
+        <SubHeader label="personal details" />
+        <FormGrid>
+          <Field label="GIVEN NAME" name="givenName" />
+          <Field label="SURNAME" name="surname" />
+          <Field label="EMAIL" name="email" />
+          <Field label="PHONE" name="phone" />
+        </FormGrid>
+        <SubHeader label="address" />
+        <FormGrid>
+          <Field label="HOME NAME OR #" name="givenName" />
+          <Field label="STREET" name="givenName" />
+          <Field label="suburb" name="givenName" />
+          <Field label="state" name="givenName" />
+          <Field label="postcode" name="givenName" />
+          <Field label="country" name="givenName" />
+        </FormGrid>
+        <FormGrid className="mt-10">
+          <Button>upload avatar</Button>
+          <Button className="bg-[#67dc7e] text-white">create referral</Button>
+        </FormGrid>
+      </Section>
+      <Section className="bg-gray-100 px-10 py-20">
+        <div className="bg-white w-full h-full px-5 py-10">
+          <table className="w-full">
+            <thead>
+              <TableRow>
+                <TableHeader>GIVEN NAME</TableHeader>
+                <TableHeader>SURNAME</TableHeader>
+                <TableHeader>EMAIL</TableHeader>
+                <TableHeader>PHONE</TableHeader>
+                <TableHeader>ACTIONS</TableHeader>
+              </TableRow>
+            </thead>
+            <tbody>
+              <TableRow>
+                <TableCell>John</TableCell>
+                <TableCell>Johnson</TableCell>
+                <TableCell>jh@email.com</TableCell>
+                <TableCell>1233-233-333</TableCell>
+                <TableCell>
+                  <Actions />
+                </TableCell>
+              </TableRow>
+            </tbody>
+          </table>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </Section>
     </div>
   );
 }
