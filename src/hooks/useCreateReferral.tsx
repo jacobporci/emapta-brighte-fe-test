c8061c816b96b1f6a6ca406553f27c132aa024cd
Jacob Porciuncula <jacobporci@gmail.com>
2025-03-17 11:51:25 +0800

    feat: optimistic update on create referral

--- src/hooks/useCreateReferral.tsx
+++ src/hooks/useCreateReferral.tsx
@@ -1,18 +1,26 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { axiosInstance } from "../../lib/axios";
 import { Referral } from "@prisma/client";
 
 export const useCreateReferral = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationKey: ["createReferral"],
     mutationFn: async (data: Referral) => {
       try {
         const res = await axiosInstance.post("/referral", data);
-        return res;
+        return res.data;
       } catch (error) {
         console.error(error);
         throw new Error("Something went wrong");
       }
+    },
+    onSuccess: (data) => {
+      queryClient.setQueryData(["referrals"], (oldData: Referral[]) => [
+        ...oldData,
+        data,
+      ]);
     },
   });
 };
